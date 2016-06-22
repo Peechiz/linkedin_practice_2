3da2acf42d47d03a35fc511e0b92848ca6195ea3
Chris Impicciche <peechiz@bakudan.local>
2016-06-22 15:34:31 -0500

    fixed PORT typo

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use('/', main);
 app.use('/auth', auth);
 
 // start server
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port,() => {
   console.log('Servering at ' + port);
 });
