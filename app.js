a027e054ed31302fcff0bb6a6bab7c4d7131a99a
Chris Impicciche <peechiz@bakudan.local>
2016-06-22 15:15:38 -0500

    adds working OAuth maybe

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ var express = require('express'),
     passport = require('passport'),
     LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 
+
 require('dotenv').config();
 require('locus');
 
 // view engine setup
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+// above app.use('/', routes);...
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user)
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,10 +32,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
 passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_API_KEY,
   clientSecret: process.env.LINKEDIN_SECRET_KEY,
-  callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
+  callbackURL: "http://localhost:3000/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_basicprofile'],
 }, function(accessToken, refreshToken, profile, done) {
   // asynchronous verification, for effect...
@@ -48,6 +59,6 @@ app.use('/', main);
 app.use('/auth', auth);
 
 // start server
-app.listen(process.env.port || '9001',() => {
-  console.log('The Server is OVER 9000!!!');
+app.listen(process.env.port || '3000',() => {
+  console.log('Servering at 3000');
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 var express = require('express'),
   router = express.Router(),
   knex = require('../db/knex');
-router.get('/linkedin', (req, res) => {
-  console.log('logs you into linkedin');
-})
+
+router.get('/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE'  }),
+function(req, res){
+  // The request will be redirected to LinkedIn for authentication, so this
+  // function will not be called.
+});
+
+router.get('/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 
 module.exports = router;
